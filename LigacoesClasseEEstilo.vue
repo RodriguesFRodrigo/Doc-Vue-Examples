c1d9a1f37a7766a303def3e4e27ba9a870850080
Rodrigo Rodrigues <104102666+RodriguesFRodrigo@users.noreply.github.com>
2022-12-28 23:18:45 -0300

    Ligações de classe e estilo

---
+++ LigacoesClasseEEstilo.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!');
+const isActive = ref('active');
+const isOutline = ref('outline');
+const isInputBold = ref(true);
+</script>
+
+<template>
+  <h1 :class="[isActive, isOutline]">{{ msg }}</h1>
+  <!-- verbose way -->
+  <input v-model="msg" :class="[isInputBold ? 'bold' : '', isActive]">
+  <!-- does not verbose way -->
+  <input v-model="msg" :class="[{bold: isInputBold}, isActive]">
+</template>
+
+<style>
+  .active {
+    color: green;
+  }
+  
+  .outline {
+    border: 1px solid black;
+  }
+  
+  .bold {
+    font-weight: bold;
+  }
+</style>
