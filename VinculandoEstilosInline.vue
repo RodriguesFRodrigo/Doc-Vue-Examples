384f4aecf146c96da0e46f2d9d74f40e158ef204
Rodrigo Rodrigues <104102666+RodriguesFRodrigo@users.noreply.github.com>
2022-12-28 23:26:21 -0300

    Vinculando Estilos Inline

---
+++ VinculandoEstilosInline.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+import { reactive } from 'vue'
+  
+const msg = ref('Hello World!');
+const activeColor = ref('red');
+const fontSize = ref(30);
+
+const styleObject = reactive({
+ 	color: 'red',
+  fontSize: '13px'
+})
+
+</script>
+
+<template>
+  <h1 :style="{color: activeColor, fontSize: fontSize + 'px'}">{{ msg }}</h1>
+  <!-- using object -->
+  <h1 :style="styleObject">{{ msg }}</h1>
+  <input v-model="msg">
+</template>
