f2bf7b61ae26ff0954254d7fb8399e4539cbffa3
Rodrigo Rodrigues <104102666+RodriguesFRodrigo@users.noreply.github.com>
2023-02-05 22:41:06 -0300

    Create ManipulacaoEventos

---
+++ ManipulacaoEventos.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+	  return {
+    	counter: 0
+  	}
+	},
+  methods: {
+    incrementCounter(event) {
+      // recebe automaticamente o objeto DOM Event nativo 
+      alert(event);
+      this.counter++;
+    }
+	}
+}
+</script>
+
+<template>
+  <!--
+ 		@click -> Manipuladores embutidos
+    incrementCounter -> Manipuladores de método
+  -->
+	<button @click="incrementCounter">Add 1</button>
+	<p>The button above has been clicked {{ counter }} times.</p>
+</template>
